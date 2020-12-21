f22cc3ce3bc572acd317644b20842101edb905a1
Marcin <axlii50@gmail.com>
2020-12-21 03:24:38 +0100

    account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './Components/Other/Guest/Home/home.component';
 
 import { DashboardComponent } from './Components/Other/User/Dashboard/dashboard.component';
 import { SettingsComponent } from './Components/Other/User/Settings/settings.component';
+import { AccountComponent } from './Components/Other/User/Account/account.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -36,7 +37,8 @@ export function HttpLoaderFactory(http: HttpClient) {
         DashboardComponent,
         RegisterComponent,
         HomeComponent,
-        SettingsComponent
+        SettingsComponent,
+        AccountComponent
     ],
     imports: [
         BrowserModule,
